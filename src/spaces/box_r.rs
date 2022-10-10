ccb444766f7e174b9c8047c880ae3f092baccf26
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-10-10 19:04:06 -0400

    docs(spaces): describe the purpose of each space

--- src/spaces/box_r.rs
+++ src/spaces/box_r.rs
@@ -1,8 +1,13 @@
 use derive_new::new;
 use serde::Serialize;
 
+/// Defines a subspace created between two points.
 #[derive(Debug, Serialize, new, Clone)]
 pub struct BoxR<T> {
+    /// Defines the lower bound of the subspace where values less than what
+    /// is inputted cannot exist.
     pub low: T,
+    /// Defines the upper bound of the subspace where values greater than what is
+    /// inputted cannot exist.
     pub high: T,
 }
--- src/spaces/discrete.rs
+++ src/spaces/discrete.rs
@@ -3,6 +3,10 @@ use serde::Serialize;
 use super::Space;
 
 #[derive(Debug, Serialize, PartialEq, PartialOrd, Eq, Ord, Clone)]
+/// Defines a set of discrete natural numbers
+///
+/// The value held by this structure defines the largest inclusive value that
+/// exists within the derived set.  
 pub struct Discrete(pub usize);
 
 impl Space<usize> for Discrete {
