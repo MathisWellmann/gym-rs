709836a5c8b95823a1cbeba47e15f317feb7f9d7
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-10-18 18:02:25 -0400

    refactor: move example tests to examples

--- src/envs/classical_control/cartpole.rs
+++ src/envs/classical_control/cartpole.rs
@@ -169,7 +169,6 @@ impl CartPoleEnv {
         let cartwidth = OrderedFloat(50.);
         let cartheight = OrderedFloat(30.);
 
-
         screen.draw_on_canvas(
             |canvas| {
                 canvas.set_draw_color(pixels::Color::WHITE);
@@ -554,43 +553,5 @@ impl EnvProperties for CartPoleEnv {
 
     fn observation_space(&self) -> &Self::ObservationSpace {
         &self.observation_space
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use log::debug;
-    use rand::{thread_rng, Rng};
-
-    use super::CartPoleEnv;
-    use super::*;
-
-    #[test]
-    fn test_run() {
-        pretty_env_logger::try_init().unwrap_or(());
-
-        let mut env = CartPoleEnv::new(RenderMode::Human);
-        env.reset(None, false, None);
-
-        let mut rewards = vec![];
-
-        for _ in 0..15 {
-            let mut current_reward = OrderedFloat(0.);
-
-            for _ in 0..475 {
-                let action = (&mut thread_rng()).gen_range(0..=1);
-                let state_reward = env.step(action);
-                current_reward += state_reward.reward;
-
-                if state_reward.done {
-                    break;
-                }
-            }
-
-            env.reset(None, false, None);
-            rewards.push(current_reward);
-        }
-
-        debug!("{:?}", rewards)
     }
 }
